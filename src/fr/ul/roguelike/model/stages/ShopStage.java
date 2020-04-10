3ec3bb522609877ef2e214a58ca6a1f9244494c8
ProjectRedMonkey <monkeydzoror@gmail.com>
2020-04-10 09:58:51 +0200

    Création d'une première map

--- src/fr/ul/roguelike/model/stages/ShopStage.java
+++ src/fr/ul/roguelike/model/stages/ShopStage.java
@@ -2,6 +2,7 @@ package fr.ul.roguelike.model.stages;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import fr.ul.roguelike.model.GameWorld;
@@ -10,22 +11,20 @@ public class ShopStage extends Stage {
 
     public ShopStage(GameWorld gameWorld, Vector2 pos){
         super(gameWorld, pos);
-
+        texture = new Texture(Gdx.files.internal("images/shop.png"));
+        texturePassed = new Texture(Gdx.files.internal("images/shop_passed.png"));
+        sprite = new Sprite(texture);
+        sprite.setPosition(this.getPosition().x - getRayon(), this.getPosition().y - getRayon());
+        sprite.setSize(getRayon() * coeff, getRayon() * coeff);
     }
 
-    public void draw(SpriteBatch spriteBatch){
-        if(actual){
+    public void draw(SpriteBatch spriteBatch) {
+        if (actual) {
             coeff = 3;
         }
-        if(passed) {
-            spriteBatch.draw(new Texture(Gdx.files.internal("images/shop_passed.png"))
-                    , this.getPosition().x - getRayon(), this.getPosition().y - getRayon(),
-                    getRayon() * coeff, getRayon() * coeff);
-
-        }else{
-            spriteBatch.draw(new Texture(Gdx.files.internal("images/shop.png"))
-                    , this.getPosition().x - getRayon(), this.getPosition().y - getRayon(),
-                    getRayon() * coeff, getRayon() * coeff);
+        if (passed) {
+            sprite.setTexture(texturePassed);
         }
+        sprite.draw(spriteBatch);
     }
 }
--- src/fr/ul/roguelike/views/MapInterface.java
+++ src/fr/ul/roguelike/views/MapInterface.java
@@ -1,10 +1,12 @@
 package fr.ul.roguelike.views;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
@@ -28,7 +30,8 @@ public class MapInterface extends ScreenAdapter {
     static int i = 0;
     static int j = -1;
     static boolean bool = false;
-    static Stage stageD = null, stageG;
+    static Stage stageD, stageG, stageU, tamponD, tamponG;
+    private int coeff = 100;
 
     public MapInterface(){
         spriteBatch = new SpriteBatch();
@@ -70,11 +73,23 @@ public class MapInterface extends ScreenAdapter {
                 shapeRenderer.line(stage.getPosition(), stage.getLeftStage().getPosition());
                 shapeRenderer.end();
             }
+            if(stage.getUniqueStage() != null) {
+                shapeRenderer = new ShapeRenderer();
+                shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+                shapeRenderer.setColor(0, 0, 0, 1);
+                shapeRenderer.line(stage.getPosition(), stage.getUniqueStage().getPosition());
+                shapeRenderer.end();
+            }
         }
 
         //Dessin des icônes de stage
         spriteBatch.begin();
         for (Stage stage: listeStages) {
+            if(stage.getSprite().getBoundingRectangle().contains(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY())){
+                if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT) || Gdx.input.isTouched()) {
+                    stage.setPassed(true);
+                }
+            }
             stage.draw(spriteBatch);
         }
 
@@ -89,6 +104,9 @@ public class MapInterface extends ScreenAdapter {
 
     }
 
+    /**
+     * Appelle les fonctions qui génère l'arbre
+     */
     private void generateMap(){
         int x = 200;
         int y = 450;
@@ -96,50 +114,114 @@ public class MapInterface extends ScreenAdapter {
         tampon = new CombatStage(gameWorld, new Vector2(x, y));
         listeStages.add(tampon);
         buildMap(tampon);
+
+        //Fin de la map par un boss
         stage = new BossStage(gameWorld, new Vector2(x + 1050 , y));
         listeStages.add(stage);
-        stageD.setRightStage(stage, camera);
-        stageG.setRightStage(stage, camera);
-
         Stage s;
         for(int i = 0 ; i < listeStages.size() ; i++){
+            s = listeStages.get(i);
+            if(s.getRightStage() == null && s.getLeftStage() == null && s.getUniqueStage() == null){
+                s.setUniqueStage(stage);
+            }
+        }
+
+        /*for(int i = 0 ; i < listeStages.size() ; i++){
             s = listeStages.get(i);
             if(s.getRightStage() == null && s.getLeftStage() == null){
                 if(i+2 < listeStages.size()) {
                    s.setRightStage(listeStages.get(i + 2), camera);
                 }
             }
-        }
+        }*/
     }
 
+
+    /**
+     * Construit l'arbre à partir de la racine
+     * @param stage racine
+     */
     private void buildMap(Stage stage){
-        //Fils droit
-        stageD = stageAleatoire(new Vector2(stage.getPosition().x + 80, stage.getPosition().y - 80));
-        listeStages.add(stageD);
-        stage.setRightStage(stageD, camera);
+        int rand = 1;
 
+        switch(rand){
+            //Première map
+            case 1:
+            createRightStage(stage);
+            createLeftStage(stage);
+            tamponD = stageD;
+            tamponG = stageG;
+            createRightStage(tamponD);
+            createLeftStage(tamponD);
 
-        //Fils gauche
-        stageG = stageAleatoire(new Vector2(stage.getPosition().x  + 80, stage.getPosition().y + 80));
-        listeStages.add(stageG);
-        stage.setLeftStage(stageG, camera);
-        if(j == -1) {
-            j++;
-            buildMap(stageD);
+            tamponG.setRightStage(stageG);
+            createLeftStage(tamponG);
+            createUniqueStage(tamponG.getRightStage());
+            createUniqueStage(stageU);
+            tamponG = stageU;
+
+            createUniqueStage(stageD);
+            createUniqueStage(stageU);
+            createUniqueStage(stageU);
+            tamponD = stageU;
+            createUniqueStage(stageG);
+            createUniqueStage(stageU);
+            createRightStage(stageU);
+            tamponG.setLeftStage(stageD);
+            createRightStage(stageD);
+            tamponD.setLeftStage(stageD);
+            createLeftStage(stageD);
+            createRightStage(stageD);
+            createUniqueStage(stageD);
+            createUniqueStage(stageG);
+            break;
         }
-        if(j < 10) {
-            j++;
-            if(j%2 == 0) {
-                buildMap(stageD);
-            }else{
-                buildMap(stageG);
+
+    }
+
+    /**
+     * Indique si un stage est déjà présent
+     * @param position du stage que l'on souhaite vérifier
+     * @return true si un stage est déjà présent
+     */
+    private boolean isStagePresent(Vector2 position){
+        boolean res = false;
+        for(Stage stage : listeStages){
+            if(stage.getPosition().equals(position)){
+                res = true;
             }
         }
+        return res;
     }
 
+    private void createRightStage(Stage stage){
+        //Fils droit
+        stageD = stageAleatoire(new Vector2(stage.getPosition().x + coeff, stage.getPosition().y - coeff));
+        listeStages.add(stageD);
+        stage.setRightStage(stageD);
+    }
+
+    private void createUniqueStage(Stage stage){
+        //Fils droit
+        stageU = stageAleatoire(new Vector2(stage.getPosition().x + coeff+20, stage.getPosition().y));
+        listeStages.add(stageU);
+        stage.setUniqueStage(stageU);
+    }
+
+    private void createLeftStage(Stage stage){
+        //Fils gauche
+        stageG = stageAleatoire(new Vector2(stage.getPosition().x + coeff, stage.getPosition().y + coeff));
+        listeStages.add(stageG);
+        stage.setLeftStage(stageG);
+    }
+
+    /**
+     * Crée un stage aléatoirement parmi les 4 existants
+     * @param position du stage que l'on veut créer
+     * @return le nouveau stage
+     */
     private Stage stageAleatoire(Vector2 position){
         int max = 6;
-        int min = 0;
         Random r = new Random();
         int rand = r.nextInt(max);
         Stage stage = null;
