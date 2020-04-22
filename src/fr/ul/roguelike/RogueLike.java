3210e97ff4c3883eb8004383618eba9f12e0f288
MrYdag <ydag88@gmail.com>
2020-04-22 15:48:52 +0200

    add MainMenu and link

--- src/fr/ul/roguelike/RogueLike.java
+++ src/fr/ul/roguelike/RogueLike.java
@@ -2,15 +2,15 @@ package fr.ul.roguelike;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import fr.ul.roguelike.views.MainMenu;
 import fr.ul.roguelike.views.MapInterface;
 import fr.ul.roguelike.views.SplashScreen;
 
 import com.badlogic.gdx.utils.Timer;
 
 public class RogueLike extends Game {
-	SpriteBatch batch;
-	SplashScreen splashScreen;
-	MapInterface map;
+	private SplashScreen splashScreen;
+	private MainMenu mainMenu;
 
 	@Override
 	public void create () {
@@ -28,12 +28,11 @@ public class RogueLike extends Game {
 
     private void changeScreen(){
         splashScreen.dispose();
-        map = new MapInterface(this);
-        setScreen(map);
+        mainMenu = new MainMenu(this);
+        setScreen(mainMenu);
     }
 
 	@Override
 	public void dispose () {
-		batch.dispose();
 	}
 }
--- src/fr/ul/roguelike/views/MainMenu.java
+++ src/fr/ul/roguelike/views/MainMenu.java
@@ -1,6 +1,108 @@
 package fr.ul.roguelike.views;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import fr.ul.roguelike.RogueLike;
+import fr.ul.roguelike.model.Items.ButtonItem;
 
 public class MainMenu extends ScreenAdapter {
+
+    private MapInterface mapInterface;
+    private SpriteBatch spriteBatch;
+    private Stage stage;
+    private OrthographicCamera camera;
+    private RogueLike rogueLike;
+
+    private Button playButton,exitButton;
+    private Texture texture;
+
+    private int largeurEcran = Gdx.graphics.getWidth();
+    private int hauteurEcran = Gdx.graphics.getHeight();
+    private int largeurBouton = Gdx.graphics.getWidth()/5;
+    private int hauteurBouton = Gdx.graphics.getWidth()/9;
+
+    public MainMenu(final RogueLike rogueLike){
+        stage = new Stage();
+        this.rogueLike = rogueLike;
+        mapInterface = new MapInterface(rogueLike);
+        Gdx.input.setInputProcessor(stage);
+        spriteBatch = new SpriteBatch();
+        Image fond = new Image(new Texture(Gdx.files.internal("images/menu.png")));
+        fond.setWidth(largeurEcran);
+        fond.setHeight(hauteurEcran);
+        stage.addActor(fond);
+
+        texture = new Texture(Gdx.files.internal("images/woodTexture.png"));
+        Drawable drawable = new TextureRegionDrawable(new TextureRegion(texture));
+        drawable.setMinHeight(hauteurBouton);
+        drawable.setMinWidth(largeurBouton);
+        playButton = new ButtonItem(drawable);
+        playButton.setSize(largeurBouton,hauteurBouton);
+        playButton.setPosition(largeurEcran/2-largeurBouton/2,hauteurEcran/2);
+        playButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                play();
+            };
+        });
+
+        texture = new Texture(Gdx.files.internal("images/woodTexture.png"));
+        Drawable drawable2 = new TextureRegionDrawable(new TextureRegion(texture));
+        drawable2.setMinHeight(hauteurBouton);
+        drawable2.setMinWidth(largeurBouton);
+        exitButton = new ButtonItem(drawable2);
+        exitButton.setSize(largeurBouton,hauteurBouton);
+        exitButton.setPosition(largeurEcran/2-largeurBouton/2,hauteurEcran/2-hauteurEcran/4);
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            };
+        });
+
+
+        stage.addActor(playButton);
+        stage.addActor(exitButton);
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false,largeurEcran,hauteurEcran);
+        camera.update();
+    }
+
+    public void play (){
+        mapInterface = new MapInterface(rogueLike);
+        rogueLike.setScreen(mapInterface);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        spriteBatch.setProjectionMatrix(camera.combined);
+        stage.draw();
+        stage.act();
+        camera.update();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+
+    public MapInterface getMap() {
+        return mapInterface;
+    }
 }
