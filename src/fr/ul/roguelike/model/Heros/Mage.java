18c5fe017dbd8b694c5730bcc59dce40fc0a6a36
ProjectRedMonkey <monkeydzoror@gmail.com>
2020-04-29 16:17:09 +0200

    correcting bug

--- src/fr/ul/roguelike/model/Heros/Mage.java
+++ src/fr/ul/roguelike/model/Heros/Mage.java
@@ -10,10 +10,10 @@ public class Mage extends Hero {
         nb_spell_slots = 2;
 
 
-        animAttack = new Animation<Texture>(0.075f,loadFrames(19,"images/combat/Mage/Attack/mage_attack_"));
-        animBlock = new Animation<Texture>(0.1f,loadFrames(4,"images/combat/Warrior/Block/HeroKnight_Block_"));
-        animDead = new Animation<Texture>(0.1f,loadFrames(13,"images/combat/Mage/Death/mage_death_"));
-        animIdle = new Animation<Texture>(0.1f,loadFrames(10,"images/combat/Mage/Idle/mage_idle"));
+        animAttack = new Animation<Texture>(0.075f,loadFrames(20,"images/combat/Mage/Attack/mage_attack_"));
+        animBlock = new Animation<Texture>(0.075f,loadFrames(13,"images/combat/Mage/Block/mage_block_"));
+        animDead = new Animation<Texture>(0.1f,loadFrames(14,"images/combat/Mage/Death/mage_death_"));
+        animIdle = new Animation<Texture>(0.1f,loadFrames(11,"images/combat/Mage/Idle/mage_idle"));
 
         width = animIdle.getKeyFrame(0,false).getWidth()/1.3f;
         height = animIdle.getKeyFrame(0,false).getHeight()/1.3f;
